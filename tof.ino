a7de1f5336ab20b6e4a5e84d0175079341f29b89
rlatn10 <48874959+rlatn10@users.noreply.github.com>
2020-05-21 15:26:35 +0900

    Update tof.ino
    add arduino example 'servo-sweep' to tof code

--- tof.ino
+++ tof.ino
@@ -1,37 +1,48 @@
-/* This example shows how to use continuous mode to take
-range measurements with the VL53L0X. It is based on
-vl53l0x_ContinuousRanging_Example.c from the VL53L0X API.
-
-The range readings are in units of mm. */
-
 #include <Wire.h>
-#include <VL53L0X.h>
+#include <VL53L0X.h> 
+#include <Servo.h>  
 
-VL53L0X sensor;
-  
+VL53L0X sensor; 
+Servo myservo;
+int servoPin = 9; 
+int pos = 0;      //서보모터 각을 저장할 변수
+int stop_state=0; //이미 멈추었는지를 나타내줄 변수
 void setup()
 {
   Serial.begin(9600);
   Wire.begin();
-
+ myservo.attach(9);  // attaches the servo on pin 9 to the servo object
   sensor.setTimeout(500);
   if (!sensor.init())
   {
     Serial.println("Failed to detect and initialize sensor!");
     while (1) {}
   }
-
-  // Start continuous back-to-back mode (take readings as
-  // fast as possible).  To use continuous timed mode
-  // instead, provide a desired inter-measurement period in
-  // ms (e.g. sensor.startContinuous(100)).
   sensor.startContinuous();
 }
-
 void loop()
 {
-  Serial.print(sensor.readRangeContinuousMillimeters());
-  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
-
+  Serial.print(sensor.readRangeContinuousMillimeters());  //측정된 거리 시리얼 모니터에 출력
+  if (sensor.timeoutOccurred()) 
+  { Serial.print(" TIMEOUT"); }
   Serial.println();
+  if(sensor.readRangeContinuousMillimeters() > 700 && stop_state == 0)  //측정된 거리가 설정 값보다 크고 stop_state가 0일때 멈춤
+    {
+       for (pos = 0; pos <= 90; pos += 1) 
+       { // goes from 0 degrees to 180 degrees
+        // in steps of 1 degree
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(10);                       // waits 15ms for the servo to reach the position
+       }
+       for (pos = 90; pos >= 0; pos -= 1) 
+       { // goes from 180 degrees to 0 degrees
+       myservo.write(pos);              // tell servo to go to position in variable 'pos'
+       delay(3);                       // waits 15ms for the servo to reach the position
+       }
+    stop_state = 1;                   //이미 멈추었으니 stop_state를 1로 바꿔줌 
+    }
+ if(sensor.readRangeContinuousMillimeters() < 700)   //측정된 거리가 설정 값보다 작다면 stop_state에 0을 넣어줌
+ {
+  stop_state = 0;
+  }
 }
